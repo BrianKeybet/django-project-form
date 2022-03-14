6fc1f8c083e034b6665d1d995c0eed4a9848709d
BrianKeybet <86772792+BrianKeybet@users.noreply.github.com>
2022-03-14 23:16:25 +0300

    Updated settings to remove secret key

--- django_project_form/settings.py
+++ django_project_form/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jfu%ft-hi0su-0gq192n^lp@#m(@=$jaj)h7t4@%_%-=gjxgpe'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
