85be63c75ebcd3b9c109a075a0b373cfc906a6b8
BrianKeybet <86772792+BrianKeybet@users.noreply.github.com>
2022-05-10 14:31:49 +0300

    Grid Layout

--- waste_management/views.py
+++ waste_management/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import waste_delivery_note
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from datetime import date
 
 # Create your views here.
-class waste_delivery_noteCreateView(SuccessMessageMixin, generic.CreateView):
+class waste_delivery_noteCreateView(LoginRequiredMixin, SuccessMessageMixin, generic.CreateView):
+    today = date.today()
     template_name = 'waste_management/waste_dnote.html'
     success_message = 'Form Submitted Sccessfully!'
     model = waste_delivery_note
